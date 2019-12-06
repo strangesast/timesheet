5af6b609c7d9cb219fef8e6911b7ac6c063a6b88
Sam Zagrobelny <strangesast@gmail.com>
2019-12-06 09:59:05 -0500

    try that

--- main.py
+++ main.py
@@ -1,118 +1,153 @@
+''' scrape google timeline location xml for work start / end
+'''
 import os
+import math
 import urllib.parse
 import sqlite3
+import contextvars
+import argparse
 import contextlib
 from datetime import date, timedelta, datetime, timezone
 from win32.win32crypt import CryptUnprotectData
-from requests import get, Request, Session
+from requests import Request, Session
 from bs4 import BeautifulSoup
 
+TIMELINE_URL = 'https://www.google.com/maps/timeline/kml'
+USE_CACHE = contextvars.ContextVar('USE_CACHE', default=False)
 
-def rewrite_daterange_string(a, b):
-    ''' query parameter format for date range. only a single day works
+def valid_date(date_s: str):
+    ''' parse date from args
     '''
-    return f'1m8!1m3!1i{a.year}!2i{a.month - 1}!3i{a.day}!2m3!1i{a.year}!2i{a.month - 1}!3i{a.day}'
+    try:
+        return datetime.strptime(date_s, '%m/%d/%y')
+    except ValueError:
+        msg = f'Not a valid date: "{date_s}""'
+        raise argparse.ArgumentTypeError(msg)
 
-def get_cookies():
-    ''' extract cookies for timeline from chrome.  (TODO: fix platform dependency)
+
+def rewrite_daterange_string(from_date, to_date):
+    ''' generate valid params for maps request
     '''
-    cookiePath = os.path.join(os.environ['localappdata'], 'Google\\Chrome\\User Data\\Default\\Cookies')
-    cookies = {}
-    with contextlib.closing(sqlite3.connect(cookiePath)) as con:
-        cur = con.execute('''select name, encrypted_value from cookies
-                             where host_key like "%google.com"''')
-        
-        for key, encrypted_value in cur:
-            _, value = CryptUnprotectData(encrypted_value, None, None, None, 0)
-            cookies[key] = value.decode()
-
-    return cookies
+    return (
+        f'1m8!1m3!1i{from_date.year}!2i{from_date.month - 1}!3i'
+        f'{from_date.day}!2m3!1i{to_date.year}!2i{to_date.month - 1}'
+        f'!3i{to_date.day}'
+        )
 
 
-session = Session()
-cookies = get_cookies()
-localtz = timezone(datetime.fromtimestamp(0) - datetime.utcfromtimestamp(0))
-
-def check_cache(req):
-    ''' check if request with same url / query parameters was made before
-        if not, make it.  save response content.  return response content
+def set_cookies(session: Session):
+    ''' read cookies from chrome sqlite db
     '''
-    prepped = session.prepare_request(req)
-    url = urllib.parse.quote(prepped.url, '') # remove slashes etc
-    if not os.path.isdir('cache'):
-        os.mkdir('cache')
+    cookie_path = os.path.join(
+        os.environ['localappdata'],
+        'Google\\Chrome\\User Data\\Default\\Cookies')
+    with contextlib.closing(sqlite3.connect(cookie_path)) as con:
+        cur = con.execute('''
+            select name, encrypted_value from cookies
+            where host_key like "%google.com"''')
 
-    path = os.path.join('cache', url)
+        for name, encrypted_value in cur:
+            value = CryptUnprotectData(encrypted_value, None, None, None, 0)[1]
+            if name is bytes:
+                name = name.decode()
+            cookie = {
+                "name": name,
+                "value": value.decode()
+                }
+            session.cookies.set(**cookie)
 
-    if os.path.exists(path): # cache directory must be present
-        with open(path, 'r') as f:
-            return f.read()
-    res = session.send(prepped)
-    content = res.content.decode()
-    if (code := res.status_code) >= 200 and code < 400:
-        with open(path, 'w') as f:
-            f.write(content)
-    else:
+
+SESSION = Session()
+set_cookies(SESSION)
+LOCALTZ = timezone(datetime.fromtimestamp(0) - datetime.utcfromtimestamp(0))
+PARSER = argparse.ArgumentParser(description="scrape google timeline for work hours")
+PARSER.add_argument(
+        '--date',
+        metavar='D',
+        help="Date in week of requested timeline (MM/DD/YYYY)",
+        required=False,
+        type=valid_date)
+PARSER.add_argument('--cache', help="Cache timeline responses", required=False, type=bool)
+
+
+def make_request(params) -> str:
+    ''' make an http request with params to timeline url
+    '''
+    cache = USE_CACHE.get()
+    req = Request('GET', TIMELINE_URL, params=params)
+    prepped = SESSION.prepare_request(req)
+    if cache:
+        url = urllib.parse.quote(prepped.url, '')
+        path = os.path.join('cache', url)
+        if os.path.exists(path):
+            with open(path, 'r') as fobj:
+                return fobj.read()
+
+    res = SESSION.send(prepped)
+    if (code := res.status_code) < 200 or code > 400:
         raise Exception(f'failed to fetch {url}')
+
+    content = res.content.decode()
+    if cache:
+        with open(path, 'w') as fobj:
+            fobj.write(content)
     return content
 
 
-def round_to(f, frac=4):
-    ''' round float to quarters / halves
+def parse_date_str(date_s):
+    ''' parse date like 2019-11-26T01:35:36.088Z
     '''
-    return int(f * frac) / frac
+    return datetime.fromisoformat(date_s.replace("Z", "+00:00"))
 
 
-def parse_datetime(s):
-    return datetime.fromisoformat(s.replace("Z", "+00:00")) 
-
-def get_timeline_for_day(date, filter=lambda s: s.startswith('Work')):
-    ''' request timeline kml for date, return Points matching filter
+def round_hours(delta: timedelta, frac=4):
+    ''' round hours to quarters / halfs etc
     '''
-    a = date
-    b = date + timedelta(days=1)
-    
-    params = 'authuser=0&pb=' + rewrite_daterange_string(a, b)
-    req = Request('GET', 'https://www.google.com/maps/timeline/kml', cookies=cookies, params=params)
-    content = check_cache(req)
-    
-    soup = BeautifulSoup(content, 'xml')
-    for pm in soup.find_all('Placemark'):
-        if (point := pm.find('Point')):
-            name = pm.find('name').string
-            if filter(name):
-                ts = pm.find('TimeSpan')
-                start, end = [parse_datetime(ts.find(k).string) for k in ('begin', 'end')]
-                yield (start, end)
-       
+    hrs = delta / timedelta(hours=1)
+    return (math.floor(hrs * frac) / frac, hrs)
 
-def get_timeline_for_week(date):
-    ''' print hours for each day in week of date
+
+def get_timeline(target_date):
+    ''' get timeline for a day
     '''
-    start = date - timedelta(days=date.weekday())
+    params = 'authuser=0&pb=' + rewrite_daterange_string(
+        target_date,
+        target_date + timedelta(days=1))
+    content = make_request(params)
+    soup = BeautifulSoup(content, 'lxml-xml')
+    for x_placemark in soup.find_all('Placemark'):
+        if x_placemark.find('Point'):
+            name = x_placemark.find('name').string
+            x_time_span = x_placemark.find('TimeSpan')
+            start, end = [parse_date_str(x_time_span.find(s).string) for s in ('begin', 'end')]
+            yield (name, start, end)
+
+
+def get_timeline_for_week(target_date):
+    ''' print start, duration for each day in target week
+    '''
+    start = target_date - timedelta(days=target_date.weekday())
     days = [start + timedelta(days=i) for i in range(0, 5)]
 
     print(' - '.join(d.strftime('%a, %m/%d/%y') for d in (days[0], days[-1])))
     print()
-    for date in days:
-        print(date.strftime('%m/%d/%y'))
-        groups = list(get_timeline_for_day(date))
+    for each_date in days:
+        print(each_date.strftime('%m/%d/%y'))
+        timeline = get_timeline(each_date)
         tot = 0
-        for start, end in groups:
-            s = ' - '.join(d.astimezone(localtz).strftime('%I:%M%p') for d in (start, end))
-            print(s)
-            f = (end - start) / timedelta(hours=1)
-            g = round_to(f)
-            print(f'{g} ({f:.2f})')
-            tot += f
-
-        if (l := len(groups)) == 0:
-            print(None)
-        elif l > 1:
-            print(f'total: {round_to(tot)}')
+        for name, start, end in timeline:
+            if name.startswith('Work'):
+                print(' - '.join(d.astimezone(LOCALTZ).strftime('%I:%M%p') for d in (start, end)))
+                delta = end - start
+                rounded, exact = round_hours(delta)
+                tot += exact
+                print(f'{rounded} ({exact:.2f})')
+        print(f'total: {tot:.2f}')
         print()
 
 
 if __name__ == '__main__':
-    now = date.today() - timedelta(7)
-    get_timeline_for_week(now)
+    ARGS = PARSER.parse_args()
+    USE_CACHE.set(ARGS.cache or False)
+    TARGET_DATE = ARGS.date or (date.today() - timedelta(7))
+    get_timeline_for_week(TARGET_DATE)
